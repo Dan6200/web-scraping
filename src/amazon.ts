d7579b2729deab3fa2e603a895b1002a3393d7a1
Daniel Nyong <danielnyong66@gmail.com>
2023-07-17 10:36:45 +0100

    refactored, prettier, and added persist-data

--- src/amazon.ts
+++ src/amazon.ts
@@ -1,52 +1,52 @@
 //cspell:disable
-import puppeteer from 'puppeteer-extra'
-import stealth from 'puppeteer-extra-plugin-stealth'
-import { getSubLinks } from './supporting-funcs.js'
-import fs from 'node:fs'
-import { delay, visitSubLinks } from './supporting-funcs.js'
-import chai from 'chai'
+import puppeteer from "puppeteer-extra";
+import stealth from "puppeteer-extra-plugin-stealth";
+import { getSubLinks } from "./supporting-funcs.js";
+import fs from "node:fs";
+import { delay, visitSubLinks } from "./supporting-funcs.js";
+import chai from "chai";
 
-chai.should()
+chai.should();
 
 // Use Stealth plugin to avoid Captcha
-puppeteer.default.use(stealth())
+puppeteer.default.use(stealth());
 
 export async function scrape() {
-  let browser: any
-  let fileStream: fs.WriteStream | undefined
+  let browser: any;
+  let fileStream: fs.WriteStream | undefined;
 
   // Adding 's' to the end of the url somehow avoids Captcha
-  const baseUrl = 'https://www.amazon.com/s'
+  const baseUrl = "https://www.amazon.com/s";
 
   try {
     browser = await puppeteer.default.launch({
-      headless: 'new',
-      // headless: false,
+      // headless: 'new',
+      headless: false,
       args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--window-size=1920,1080',
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--window-size=1920,1080",
       ],
       ignoreHTTPSErrors: true,
-      executablePath: '/usr/bin/google-chrome-stable',
-    })
+      executablePath: "/usr/bin/google-chrome-stable",
+    });
 
-    const page = await browser.newPage()
+    const page = await browser.newPage();
 
     // Get sublink from hamburger menu
-    const subLinks = await getSubLinks(page, baseUrl)
+    const subLinks = await getSubLinks(page, baseUrl);
 
     // wait
-    delay(5000)
+    delay(5000);
 
     // prints the number of sublinks
-    console.log(subLinks.length)
+    console.log(subLinks.length);
 
     // Create a filestream to write data to a file
-    fileStream = fs.createWriteStream('data.json')
+    fileStream = fs.createWriteStream("data.json");
 
     // Store data in an array
-    fileStream.write('[\n')
+    fileStream.write("[\n");
 
     // Visits each sublink and scrapes data
     for await (const data of visitSubLinks(
@@ -55,28 +55,28 @@ export async function scrape() {
       subLinks as string[]
     )) {
       // print data to console
-      console.log(data)
+      console.log(data);
       // write data to file
-      fileStream.write(JSON.stringify(data) + ',\n')
+      fileStream.write(JSON.stringify(data) + ",\n");
     }
   } catch (err) {
-    console.log('Operation failed', err)
+    console.log("Operation failed", err);
   } finally {
     // Clean up
-    fileStream?.write(']\n')
-    fileStream?.end()
-    await browser?.close()
+    fileStream?.write("]\n");
+    fileStream?.end();
+    await browser?.close();
   }
 }
 
 async function callScrape() {
   try {
-    await scrape()
+    await scrape();
   } catch (error) {
-    console.log(error)
-    delay(10_000)
-    await scrape()
+    console.log(error);
+    delay(10_000);
+    await scrape();
   }
 }
 
-callScrape()
+callScrape();
